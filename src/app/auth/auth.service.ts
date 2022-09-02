4e63a8dd77ee11bbddb9778df39ce4ff2676af54
kevit-megha-rana <megha.rana@kevit.io>
2022-09-02 18:38:18 +0530

    final commit

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -63,6 +63,30 @@ export class AuthService{
     logout(){
         this.user.next(null);
         this.router.navigate(['/auth']);
+        localStorage.removeItem('userData');
+    }
+
+    autoLogin(){
+        const userData: {
+            email:string;
+            id:string;
+            _token:string;
+            _tokenExpirationDate: string;
+        } = JSON.parse(localStorage.getItem('userData'));
+        if(!userData){
+            return;
+        }
+
+        const loadedUser = new User(
+            userData.email,
+            userData.id,
+            userData._token,
+            new Date(userData._tokenExpirationDate)
+            );
+
+        if(loadedUser.token){
+            this.user.next(loadedUser);
+        }
     }
 
     private handleAuthentication(email:string,userId:string,token:string,expiresIn:number){
@@ -76,5 +100,6 @@ export class AuthService{
             expirationDate
         );
         this.user.next(user);
+        localStorage.setItem('userData',JSON.stringify(user));
     }
 }
